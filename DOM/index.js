248e44f019e1d9e9efcc8ed06673e31a63c6551e
Shadow-ninja45 <a77926148@gmail.com>
2024-02-22 10:34:31 +0530

    made corrections

--- DOM/index.js
+++ DOM/index.js
@@ -1,7 +1,7 @@
 // Write your code below:
 document.getElementById("main-heading").innerHTML="Fruit World";
 document.getElementById("header").style.backgroundColor="green";
-document.getElementById("header").style.color="orange";
+document.getElementById("main-heading").style.color="orange";
 document.getElementById("header").style.borderBottom="5px solid orange";
 document.getElementById("basket-heading").style.color="green";
 var p=document.createElement("p");
