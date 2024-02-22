a5f1514740cfe18ea824f5372f3ffce74ac91184
Shadow-ninja45 <a77926148@gmail.com>
2024-02-22 10:25:22 +0530

    Update index.js

--- DOM/index.js
+++ DOM/index.js
@@ -1,6 +1,8 @@
+// Write your code below:
 document.getElementById("main-heading").innerHTML="Fruit World";
 document.getElementById("header").style.backgroundColor="green";
-document.getElementById("header").style.borderBottomColor="orange";
+document.getElementById("header").style.color="orange";
+document.getElementById("header").style.borderBottom="5px solid orange";
 document.getElementById("basket-heading").style.color="green";
 var p=document.createElement("p");
 document.getElementById("thanks").appendChild(p).innerHTML="Please visit us again";
